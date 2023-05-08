9f2f1653ea9f6d2f7c52430fe04523837e2283c2
juanda <juandacorreo@gmail.com>
2023-05-08 10:52:51 +0200

    add phrases endpoint

--- src/api/v1/controllers/phrases.ts
+++ src/api/v1/controllers/phrases.ts
@@ -7,15 +7,39 @@ import axios from 'axios'
 
 export async function getFlex(req: express.Request, res: express.Response): Promise<void> {
   const { phrase } = req.params
+  const { tense, idPictograms } = req.query
+  let tenseParam = ''
+  // [PP] pretérito perfecto simple,  [PI] pretérito imperfecto, [F] Futuro
+
+  if (tense === 'future') tenseParam = '[F]'
+  else if (tense === 'past') tenseParam = '[PP]'
+  console.log(`idPittograms received: ${idPictograms}`)
+
   try {
-    const url = `http://arasaac_freeling:5000/flexionar?frase=${phrase}`
+    const url = `http://arasaac_freeling:5000/flexionar?frase=${tenseParam}${phrase}`
     // get data from url using axios:
     const { data } = await axios.get(url)
     writeJsonResponse(res, 200, data)
   } catch (err) {
     logger.error(`Error getting getFlex for phrase ${phrase}. See error: ${err}`)
     writeJsonResponse(res, 500, {
-      message: 'Error getting keywords. See error field for detail',
+      message: 'Error getting getFlex. See error field for detail',
+      error: err,
+    })
+  }
+}
+
+export async function postReport(req: express.Request, res: express.Response): Promise<void> {
+  const { originalPhrase, returnedPrase, expectedPhrase } = req.query
+  try {
+    console.log(`originalPhrase: ${originalPhrase}`)
+    console.log(`returnedPrase: ${returnedPrase}`)
+    console.log(`expectedPhrase: ${expectedPhrase}`)
+    writeJsonResponse(res, 200, 'ok')
+  } catch (err) {
+    logger.error(`Error getting postReport for phrase ${originalPhrase}. See error: ${err}`)
+    writeJsonResponse(res, 500, {
+      message: 'Error getting postReport. See error field for detail',
       error: err,
     })
   }
